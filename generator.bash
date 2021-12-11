4e1e0e2946cd3d9b301c4aabccbeabdb960bed2f
鲁智猫孩 <smartcatboy@tuta.io>
2021-12-11 21:36:27 +0800

    add ispip database

--- generator.bash
+++ generator.bash
@@ -5,7 +5,8 @@ if [ ! -d "$WORK_DIR/tmp" ];then
   mkdir $WORK_DIR/tmp
 fi
 
-curl -s https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt -o $WORK_DIR/tmp/all_cn.txt && \
+curl -s https://ispip.clang.cn/all_cn.txt -o $WORK_DIR/tmp/all_cn.txt && \
+curl -s https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt -o $WORK_DIR/tmp/ipip_cn.txt && \
 curl -s https://ispip.clang.cn/all_cn_ipv6.txt -o $WORK_DIR/tmp/all_cn_ipv6.txt && \
 cat > $WORK_DIR/dist/cn_ip_cidr.rsc << EOF
 /log info "Import cn ipv4 cidr list..."
@@ -13,6 +14,7 @@ cat > $WORK_DIR/dist/cn_ip_cidr.rsc << EOF
 /ip firewall address-list
 EOF
 cat $WORK_DIR/tmp/all_cn.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/dist/cn_ip_cidr.rsc && \
+cat $WORK_DIR/tmp/ipip_cn.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/dist/cn_ip_cidr.rsc && \
 cat >> $WORK_DIR/dist/cn_ip_cidr.rsc << EOF
 :if ([:len [/system package find where name="ipv6" and disabled=no]] > 0) do={
 /log info "Import cn ipv6 cidr list..."
